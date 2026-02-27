16f3801c386b6ff399d12385c64e5fef177ce3a0
PrinceMaren1 <eson@itu.dk>
2026-02-27 11:37:24 +0100

    camera position

--- player.gd
+++ player.gd
@@ -10,33 +10,12 @@ const JUMP_VELOCITY = 4.5
 @onready var camera_marker: Marker3D = $CameraMarker
 
 
-@onready var click_box: MeshInstance3D = $ClickBox
-
-
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
-	if not is_on_floor():
-		velocity += get_gravity() * delta
-
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var input_dir := Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
-	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
-	if direction:
-		velocity.x = direction.x * SPEED
-		velocity.z = direction.z * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-		velocity.z = move_toward(velocity.z, 0, SPEED)
-
-
 	var next_path_point := navigation_agent_3d.get_next_path_position()
 	var new_velocity := (next_path_point - global_position).normalized() * SPEED
 	velocity.x = new_velocity.x
 	velocity.z = new_velocity.z
-
-
+	
 	move_and_slide()
 	
 func _process(delta: float) -> void:
@@ -54,7 +33,5 @@ func move_to_click(event: InputEvent):
 	params.to = camera_3d.project_position(event.position, 1000)
 	var world_space = get_world_3d().direct_space_state
 	var cursorPos = world_space.intersect_ray(params)
-	print(cursorPos.position)
-	#click_box.global_position = cursorPos.position
 	navigation_agent_3d.target_position = cursorPos.position
 	
