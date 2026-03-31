eada60637c07817fd2a730683b3b171524c8cb1c
PrinceMaren1 <eson@itu.dk>
2026-03-31 07:11:45 +0200

    ship stats

--- Autoloads/ship_stats.gd
+++ Autoloads/ship_stats.gd
@@ -1,9 +1,47 @@
 extends Node
 
+## Ship Stuff
+
+var has_upgraded_fuel_cap = false
+
+
+var fuel = 10
+var fuel_cap = 10
+
+var damage = 0
+const severity_const = 3
+
 enum ShipStatTypes {
 	speed,
 	fuel_consumption,
 	durability,
-	maneuverabil,
+	maneuverability,
 	radiation_protection,
 }
+
+var Stats = {
+	ShipStatTypes.speed: 3,
+	ShipStatTypes.fuel_consumption: 3,
+	ShipStatTypes.durability: 3,
+	ShipStatTypes.maneuverability: 3,
+	ShipStatTypes.radiation_protection: 3,
+}
+
+func spend_fuel(fuel_spent: int): 
+	fuel -= fuel_spent
+
+func refuel():
+	fuel = fuel_cap
+	
+func repair():
+	damage = 0
+
+func upgrade_fuel_cap():
+	if !has_upgraded_fuel_cap:
+		fuel_cap = fuel_cap * 2
+
+## Event Stuff
+var rng = RandomNumberGenerator.new()
+
+func event_succes_roll(success_chance: int) -> bool:
+	return success_chance >= rng.randi_range(1,100)
