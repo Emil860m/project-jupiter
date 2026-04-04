75b21d5b2c715d2ac3b482dd331645e40b20cb12
Emil Fenger <fenger.emil@gmail.com>
2026-04-04 02:20:11 +0200

    stuff

--- Dialog/dialog_component.gd
+++ Dialog/dialog_component.gd
@@ -2,20 +2,29 @@ extends Node
 
 
 
-@export var dialogue_runner: YarnDialogueRunner
+@onready var dialogue_runner := $DialogRunner# YarnDialogueRunner
+@export var start_node: String = "Start"
+@export var auto_start: bool = false
 @onready var line_presenter := $CanvasLayer/Control/LinePresenter
 @onready var options_presenter := $CanvasLayer/Control/OptionsPresenter
 
 func _ready():
 	dialogue_runner.add_presenter(line_presenter)
 	dialogue_runner.add_presenter(options_presenter)
-	dialogue_runner.start_dialogue("Start")
-	dialogue_runner.add_function("roll", _roll, 0)
-	var coins = dialogue_runner.variable_storage.get_value("$phisical")
-	print(coins)
-	
+	add_functions()
+	if auto_start:
+		start_dialog()
 
-func _roll():
-	var i = randi_range(10, 20)
+	
+func start_dialog():
+	dialogue_runner.start_dialogue(start_node)
+
+func add_functions():
+	
+	dialogue_runner.add_function("yarn_function", godot_function, 0)
+
+
+func godot_function():
+	var i = randi_range(1, 20)
 	print(i)
 	return i 
