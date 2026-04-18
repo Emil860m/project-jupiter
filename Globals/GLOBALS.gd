764503a4edaa76df64fa3e1c760534b003d8f292
Emil Fenger <fenger.emil@gmail.com>
2026-04-18 17:45:54 +0200

    added outpost starting location

--- Globals/GLOBALS.gd
+++ Globals/GLOBALS.gd
@@ -4,7 +4,7 @@ var interacted_set = {}
 var current_timestep = 0
 @export var moon_movement_per_timestep: float = 60.0/144.0
 var max_travel_distance = 360
-var current_moon: String = "Ganymede" # todo: set default moon
+var current_moon: String = "OutPost" # todo: set default moon
 var current_location: NpcScheduler.locations = NpcScheduler.locations.HOSPITAL:
 	set(new_location):
 		SoundController.set_bgm(new_location)
