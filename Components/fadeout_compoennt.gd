165d9acd834c991d8e4d6830856bc52f49853d93
PrinceMaren1 <eson@itu.dk>
2026-04-20 11:38:53 +0200

    right fade out wall

--- Components/fadeout_compoennt.gd
+++ Components/fadeout_compoennt.gd
@@ -3,6 +3,7 @@ class_name FadeoutComponent
 
 var fade_out = false
 var mats: Array[Node]
+var collision: CollisionShape3D
 
 func _ready() -> void:
 	self.set_physics_process(false)
@@ -10,11 +11,12 @@ func _ready() -> void:
 func _physics_process(delta: float) -> void:
 	var t = delta * 0.8
 	for mat in mats:
-		if (!fade_out && mat.transparency <= 0.8 || fade_out && mat.transparency >= 0):
+		if (!fade_out && mat.transparency > 0 || fade_out && mat.transparency <= 0.8):
 			if (fade_out):
-				mat.transparency = mat.transparency + (mat.transparency + 0.1 ) * t
+				mat.transparency = mat.transparency +  0.2 * t
+				collision.disabled = true
 			if (!fade_out):
-				mat.transparency = mat.transparency - (mat.transparency) * t
-			print(mat.transparency)
+				collision.disabled = false
+				mat.transparency = mat.transparency - 0.2 * t
 		else:
 			self.set_physics_process(false)
