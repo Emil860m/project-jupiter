52dfd51f463eb68a514e42d81c6817db1e817258
PrinceMaren1 <eson@itu.dk>
2026-04-20 20:37:20 +0200

    added noice to estimated travel time and fuel

--- Scenes/Luner_system/2d/planet_view_2d.gd
+++ Scenes/Luner_system/2d/planet_view_2d.gd
@@ -11,12 +11,21 @@ var selected: Area2D
 @export var estimatedTravelLabel: Label
 @export var estimatedFuelLabel: Label
 @export var shipStatusLabel: Label
+@export var noice_max = 15.0
+@export var noice_min = 5.0
 var current_location: moon_2d
 @onready var button: Button = $UiElements/Button
 
 var event_scene := preload("res://Scenes/Luner_system/2d/event_display.tscn")
 
+var traveltime_noice = 0
+var fuel_noice = 0
+
 func _ready() -> void:
+	var travel_positive_noice = 1 == randi_range(0,1)
+	var fuel_positive_noice = 1 == randi_range(0,1)
+	traveltime_noice = randf_range(noice_min, noice_max) * (1 if travel_positive_noice else -1)
+	fuel_noice = randf_range(noice_min, noice_max) * (1 if fuel_positive_noice else -1)
 	shipStatusLabel.text = str(10 - ShipStats.damage)
 	Globals.current_location = NpcScheduler.locations.PLANET_VIEW
 	
@@ -46,8 +55,8 @@ func select_moon(hit):
 		selected = hit
 		selected.set_selected(true)
 		selectedMoonLabel.text = hit.displayName
-		estimatedTravelLabel.text = str(hit.estimated_travel_time)
-		estimatedFuelLabel.text = str(hit.estimated_travel_time)
+		estimatedTravelLabel.text = str(round(hit.estimated_travel_time * (1 + traveltime_noice / 100)))
+		estimatedFuelLabel.text = str(round(hit.estimated_travel_time * (1 + fuel_noice / 100)))
 		for m in moons.get_children():
 			m.set_estimated_loc(hit.estimated_travel_time, current_location.global_position)
 
