0970faf1b3c7853066859977fb7e70bd3af63f1a
SnowyPeax <johbr@itu.dk>
2026-04-23 20:01:34 +0200

    add bgm

--- Globals/sound_controller.gd
+++ Globals/sound_controller.gd
@@ -2,7 +2,21 @@ extends Node
 
 @onready var bgm_emitter = $BgmEmitter
 
-func set_bgm(new_bgm):
-	#bgm_emitter.set_parameter("Location", new_bgm)
-	#print(bgm_emitter.get_parameter("Location"))
-	pass
+func _ready():
+	bgm_emitter.play()
+
+func set_bgm(location):
+	bgm_emitter.set_parameter("Location", get_location_parameter(location))
+	print(bgm_emitter.get_parameter("Location"))
+
+func get_location_parameter(loc: NpcScheduler.locations) -> String:
+	match loc:
+		NpcScheduler.locations.MAIN_MENU: return "Title"
+		NpcScheduler.locations.PROLOGUE: return "Title" # TODO add prologue music
+		NpcScheduler.locations.PLANET_VIEW: return "Travel"
+		NpcScheduler.locations.PONS: return "Pons"
+		NpcScheduler.locations.RATIONALE: return "Rationale"
+		NpcScheduler.locations.WORKSHOP: return "Kallisto"
+		NpcScheduler.locations.HOSPITAL: return "Ganymede"
+		NpcScheduler.locations.OFFICE: return "Ganymede"
+		_: return "Title"
