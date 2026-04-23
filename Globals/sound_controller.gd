f7efe8f306ada9d1f47e6d412c22beb34b77b36e
SnowyPeax <johbr@itu.dk>
2026-04-23 19:45:20 +0200

    comment out redundant stuff

--- Globals/sound_controller.gd
+++ Globals/sound_controller.gd
@@ -3,5 +3,6 @@ extends Node
 @onready var bgm_emitter = $BgmEmitter
 
 func set_bgm(new_bgm):
-	bgm_emitter.set_parameter("Location", new_bgm)
-	print(bgm_emitter.get_parameter("Location"))
+	#bgm_emitter.set_parameter("Location", new_bgm)
+	#print(bgm_emitter.get_parameter("Location"))
+	pass
