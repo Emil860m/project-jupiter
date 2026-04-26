97ab3902208df4d2463f4959ae979c6b9c484ea9
PrinceMaren <ea.sondum@gmail.com>
2026-04-26 18:30:39 +0200

    unpause with escape

--- Scenes/Misc/pause_menu.gd
+++ Scenes/Misc/pause_menu.gd
@@ -11,6 +11,8 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
+	if Input.is_action_just_pressed("pause"):
+		_on_resume_button_button_up()
 	pass
 
 
