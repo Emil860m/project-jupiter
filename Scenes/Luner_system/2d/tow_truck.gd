f0a6ca6b29f8ecd2baa1bbb4521882a73c56b65b
Eliza Niedzielska <150075528+genmujmuj@users.noreply.github.com>
2026-05-07 20:30:40 +0200

    added tow truck

--- Scenes/Luner_system/2d/tow_truck.gd
+++ Scenes/Luner_system/2d/tow_truck.gd
@@ -2,10 +2,12 @@ extends Node2D
 
 var location_id: NpcScheduler.locations = NpcScheduler.locations.TOWTRUCK
 @onready var label: Label = $CanvasLayer/Label
+@onready var _animated_sprite_1 = $CanvasLayer/chuj
 
 const tow_truck_punishment = 20
 
 func _ready() -> void:
+	_animated_sprite_1.play("default")
 	if ShipStats.fuel <= 0:
 		label.text = "You did not have the necessary fuel to reach your destination.\nYour ship has been towtrucked."
 	elif ShipStats.damage > 10:
