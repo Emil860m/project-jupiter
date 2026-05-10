9e603bbc218fbc8792d2a55fe9739ff4361b7a71
Eliza Niedzielska <150075528+genmujmuj@users.noreply.github.com>
2026-05-10 17:00:20 +0200

    added animation. fixed issues in hospital scene

--- Scenes/Luner_system/2d/test_2d.gd
+++ Scenes/Luner_system/2d/test_2d.gd
@@ -1,6 +1,7 @@
 extends Node2D
 
 @onready var _animated_sprite_1 = $AnimatedSprite2D
+@onready var _animated_sprite_2 = $bootUP
 
 var location_id = NpcScheduler.locations.PLANET_VIEW
 
@@ -10,7 +11,13 @@ var location_id = NpcScheduler.locations.PLANET_VIEW
 
 func _ready() -> void:
 	_animated_sprite_1.play("default")
+	_animated_sprite_2.play("default")
 	
 	if not Flags.get_flag("ContextHasBeenRead"):
 		Flags.set_flag("ContextHasBeenRead")
 		dialog_comp.start_dialog()
+
+
+func _on_boot_up_animation_finished() -> void:
+	_animated_sprite_2.stop()
+	_animated_sprite_2.visible = false
