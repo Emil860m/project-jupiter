3c5e5b13c9ea6c8b456b7fe7f247a3248098e4e1
Emil Fenger <fenger.emil@gmail.com>
2026-05-13 23:25:47 +0200

    update ending scene to have recruited people

--- Scenes/ending_scene.gd
+++ Scenes/ending_scene.gd
@@ -1,9 +1,10 @@
 extends Node2D
 
 var location_id:= NpcScheduler.locations.ENDING
-var message := ""
+var message := "Thanks for playing!"
 var ch3: bool
 @onready var label := $CanvasLayer/Label
+@onready var label2 := $CanvasLayer/Label2
 @onready var chapterTrans = $ChapterTransition
 func _ready() -> void:
 	if ch3:
@@ -11,6 +12,7 @@ func _ready() -> void:
 	else:
 		chapterTrans.visible = false
 	label.text = message
+	label2.text = check_flags()
 	$CanvasLayer.visible = true
 func _on_mainmenu_button_up() -> void:
 	SceneController.goto_scene("res://Scenes/credits.tscn")
@@ -18,3 +20,16 @@ func _on_mainmenu_button_up() -> void:
 
 func _on_quit_button_up() -> void:
 	get_tree().root.propagate_notification(NOTIFICATION_WM_CLOSE_REQUEST)
+
+
+func check_flags() -> String:
+	var recruitedString = ""
+	if Flags.get_flag("hickRecruit"):
+		recruitedString += "Hickey "
+		if Flags.get_flag("sullyRecruit"):
+			recruitedString += "and Sully "
+	elif Flags.get_flag("sullyRecruit"):
+		recruitedString += "Sully "
+	else:
+		recruitedString += "0 people "
+	return "You recruited " + recruitedString + "to the funeral!"
