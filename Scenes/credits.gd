00276b186e0c5fc89ee16035362c783f9ffe3b67
Emil Fenger <fenger.emil@gmail.com>
2026-05-14 21:38:32 +0200

    credits updated

--- Scenes/credits.gd
+++ Scenes/credits.gd
@@ -1,6 +1,6 @@
 extends Node2D
 
-@onready var text := $CanvasLayer/RichTextLabel
+@onready var text := $CanvasLayer/Control
 var location_id := NpcScheduler.locations.MAIN_MENU
 func _on_mainmenu_button_up() -> void:
 	SceneController.goto_scene("res://Scenes/MainMenu.tscn")
@@ -8,5 +8,5 @@ func _on_mainmenu_button_up() -> void:
 
 func _process(delta: float) -> void:
 	text.global_position.y -= delta * 100
-	if text.global_position.y < -1000:
-		text.global_position.y = 1000
+	if text.global_position.y < -3500:
+		text.global_position.y = 0
